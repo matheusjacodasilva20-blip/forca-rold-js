385eab4a12e0e36319498245a22a02def1a75d9d
Matheus Jacó <matheusjacodasilva20@gmail.com>
2026-06-16 20:19:13 -0300

    banco de palavras feito, com tema de super herois

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Jogo iniciado");
+
+const palavras = [
+    "THOR",
+    "FLASH",
+    "BATMAN",
+    "AQUAMAN",
+    "DEMOLIDOR"
+];
+
+const palavra =
+    palavras[Math.floor(Math.random() * palavras.length)];
+
+console.log(palavra);
